1e530a48308bcc8630c51ecaef2a3a8e88c2bbf6
NASSER1221 <alnassri1221@gmail.com>
2025-09-24 11:50:44 +0400

    added Question 3

---
+++ AppConfig.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppConfig {
+
+    /**
+     * TO-DO: Merge 'updatedConfig' into 'mainConfig'.
+     * If a key exists in both, the value from 'updatedConfig' should be used.
+     \*
+     * @param mainConfig The primary configuration map (will be modified).
+     * @param updatedConfig The map with new or updated settings.
+     */
+    public void mergeConfigurations(Map<String, String> mainConfig, Map<String, String> updatedConfig) {
+        // Your code here. Use the correct method to merge maps.
+
+        for (String key:mainConfig.keySet()){
+
+            if (updatedConfig.containsKey(key)){
+                mainConfig.put(key, updatedConfig.get(key));
+            }
+        }
+
+
+    }
+
+}
